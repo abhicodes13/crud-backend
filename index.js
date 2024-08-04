4c822f45044bfb7cc1c29e58636e84b27f8085c3
AbhiCodes <chhettriabhishek9@gmail.com>
2024-08-04 18:43:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const productRoute = require("./routes/product.route.js");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/api/products", productRoute);
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:zWTOkJS5P1u8UbyK@backend.dxtaxn4.mongodb.net/NODE-API?retryWrites=true&w=majority&appName=Backend"
+  )
+  .then(() => {
+    console.log("Connected!");
+    app.listen(3000, () => {
+      console.log("listening..");
+    });
+  })
+  .catch(() => {
+    console.log("failed");
+  });
